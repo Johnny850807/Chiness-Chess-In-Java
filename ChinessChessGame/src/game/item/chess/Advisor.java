02f187422ad871b292d72b4c682ce61dce787a36
xuan1280 <xuan1280@gmail.com>
2017-10-15 22:21:28 +0800

    modify advisor and general

--- ChinessChessGame/src/game/item/chess/Advisor.java
+++ ChinessChessGame/src/game/item/chess/Advisor.java
@@ -14,9 +14,13 @@ public class Advisor extends Chess {
 
 	@Override
 	public boolean validateDistination(int x, int y) {
-		if (context.isInsideCastle(getColor(), x, y) && Math.abs(getX() - x) == 1 && Math.abs(getY() - y) == 1)
+		if (context.isInsideCastle(getColor(), x, y) && isMoveOblique(x, y))
 			return true;
 		return false;
 	}
 
+	private boolean isMoveOblique(int x, int y) {
+		return Math.abs(getX() - x) == 1 && Math.abs(getY() - y) == 1;
+	}
+	
 }
